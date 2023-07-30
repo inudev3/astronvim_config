2e5eedfee722a4a960abe081e9ff27736fd6091a
Inu Jung <inujung@momenti.tv>
2023-07-30 20:43:44 +0900

    momenti dark theme

--- plugins/user.lua
+++ plugins/user.lua
@@ -212,4 +212,42 @@ return {
             "nvim-lua/plenary.nvim",
         },
     },
-}
+  {
+  "olimorris/onedarkpro.nvim",
+  priority = 1000 -- Ensure it loads first
+}, 
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      integrations = {
+        alpha = true,
+        aerial = true,
+        dap = { enabled = true, enable_ui = true },
+        mason = true,
+        neotree = true,
+        notify = true,
+        nvimtree = false,
+        semantic_tokens = true,
+        symbols_outline = true,
+        telescope = true,
+        ts_rainbow = false,
+        which_key = true,
+      },
+    },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    optional = true,
+    opts = {
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+    },
+  },
+
+
+
+}--
