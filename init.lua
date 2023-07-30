17e9bc7b06100780433a8ef911f76f61c338c831
Inu Jung <inujung@momenti.tv>
2023-07-30 20:50:42 +0900

    home setup

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme =  "onedark",
+  colorscheme =  "onelight",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
